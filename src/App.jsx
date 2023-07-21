a51be0a4f6a2738ef325b7d0ef8a5f6122ab3c5b
xChoki <javier.quinmal@hotmail.cl>
2023-07-21 19:57:51 -0400

    componente botón darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Hola from './components/hola'
+import DM_Button from './components/DM_Button'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <Hola></Hola>
+      <DM_Button></DM_Button>
     </>
   )
 }
