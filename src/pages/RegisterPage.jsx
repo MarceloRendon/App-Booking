e2697323a3056e499d2128f82692f2a967672cb4
Marcelo <m.rendondemelo@gmail.com>
2023-07-24 15:35:55 -0400

    resolver-api

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,17 +1,36 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 
 export default function RegisterPage() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    function registerUser(ev){
+        ev.preventDefault();
+        axios.get('http://localhost:4000/test');
+    }
+
     return (
     <div className="mt-4 grow flex items-center justify-around">
 
         <div className=" mb-64">
             <h1 className="text-4xl text-center mb-4">Regístrate</h1>
             
-            <form className="max-w-md mx-auto" action="">
-                <input type="text" placeholder="Nombre" />
-                <input type="email" placeholder="mail@mail.com"/>
-                <input type="password" placeholder="contraseña" />
+            <form className="max-w-md mx-auto" onSubmit={registerUser}>
+                <input type="text" 
+                    placeholder="Nombre" 
+                    value={name}    
+                    onChange = {ev => setName(ev.target.value)} />
+                <input type="email" 
+                    placeholder="mail@mail.com" 
+                    value={email} 
+                    onChange = {ev => setEmail(ev.target.value)}/>
+                <input type="password" 
+                    placeholder="contraseña" 
+                    value={password} 
+                    onChange = {ev => setPassword(ev.target.value)}/>
                 <button className="buttonLogin">Registrar</button>
                 <div className="text-center py-2 text-gray-500">
                     ¿Ya tienes cuenta? <Link className="underline text-black" to={'/login'}>Ingresa aquí</Link>
