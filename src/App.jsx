d6f8d6d9f2ad6fd347fdb9a01a67ef79f119b0e6
xChoki <javier.quinmal@hotmail.cl>
2023-07-23 20:45:35 -0400

    comentado

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import COM_DMButton from './components/COM_DMButton'
 import COM_Footer from './components/COM_Footer'
 import COM_NavBar from './components/COM_NavBar'
 import { Router, Route, Routes } from 'react-router-dom'
-//<Route path="/*" element={<NotFound />} />
+{/*<Route path="/*" element={<NotFound />} />*/}
 function App() {
 
   return (
     <>
       {/* NavNar */}
       <COM_NavBar></COM_NavBar>
+      
+      {/* Filler */}
       <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-      
-      
 
       {/* Footer */}
       <COM_Footer></COM_Footer>
